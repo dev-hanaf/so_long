039e95860d6cb6545be85d3cdd6d2ce3a2c28d21
Ayoub Hanaf <ahanaf@e1r11p9.1337.ma>
2024-03-23 11:30:17 +0000

    parsing finished

--- so_long.c
+++ so_long.c
@@ -6,7 +6,7 @@
 /*   By: ahanaf <ahanaf@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/16 09:36:49 by ahanaf            #+#    #+#             */
-/*   Updated: 2024/03/23 07:51:19 by ahanaf           ###   ########.fr       */
+/*   Updated: 2024/03/23 10:01:14 by ahanaf           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +18,15 @@ char *get_all_map_lines(char **av)
     int fd = open(path ,O_RDONLY);
     char *lines = NULL;
     if (fd == - 1)
-        return NULL;
+        exit(0);
     char *str = get_next_line(fd);
     while ((str != NULL))
     {
+        if (str[0] == '\n')
+        {
+            printf("Error, Invalid map!!");
+            exit(0);
+        }
         lines = ft_strjoin(lines, str);
         str = get_next_line(fd);
     }
@@ -38,7 +43,7 @@ void valid_lenght(t_dimo *data)
     {
         if (ft_strlen(data->map[i]) != lenght)
         {
-            printf("Error, MAP SHOULD BE RECTANGULAR!!");
+            printf("Error, The map must be rectangular!!\n");
             exit(0);
         }
         i++;
@@ -62,7 +67,7 @@ void only_pce01(t_dimo *data, int i, int j)
     }
     if (!isok)
     {
-        printf("Error, Impostor character!!");
+        printf("Error, Impostor character!!\n");
         exit(1);
     }
 }
@@ -93,7 +98,7 @@ void    check_map(t_dimo *data)
         {
             if (data->map[0][j] != '1' || data->map[i][0] != '1' || data->map[i][data->width - 1] != '1' || data->map[data->height - 1][j] != '1')
             {
-                printf("map not good\n");
+                printf("The map must be closed/surrounded by walls!!\n");
                 exit(0);
             }
             only_pce01(data, i, j);
@@ -103,7 +108,7 @@ void    check_map(t_dimo *data)
     }
     if (data->p != 1 || data->c < 1 || data->e != 1)
     {
-        printf("Error, PCE Problem!!");
+        printf("Error, PCE Problem!!\n");
         exit(1);
     }
 }
@@ -112,24 +117,55 @@ void    check_map(t_dimo *data)
 int map_carre(char *str, t_dimo *data)
 {
     ft_split_so_long(str, data, '\n');
+    int i = 0;
     valid_lenght(data);
     check_map(data);
     return 0;
+}
+
+void check_map_name(char **av)
+{
+    size_t i;
+    char *path = av[1];
+    i = ft_strlen(path);
+    if (path[i - 5] != '/' && i < 5)
+    {
+        printf("Error, Ivalid path in the map!!\n");
+        exit(0);
+    }
+    if (path[i - 5] == '/' || path[i - 1] != 'r' || path[i - 2] != 'e' || path[i - 3] != 'b' || path[i - 4] != '.' ||  path[i - 5] == '.')
+    {
+        printf("Error, Ivalid path in the map!!\n");
+        exit(0);
+    }
 }
 
 int main(int ac, char **av)
 {
     t_dimo data;
     void *img;
+    void *wall;
     
     if (ac != 2)
+    {
+        printf("Takes two arguments!!\n");
         return 0;
+    }
+    check_map_name(av);
     char *str = get_all_map_lines(av);
     map_carre(str, &data);
     data.mlx = mlx_init();
     data.win = mlx_new_window(data.mlx,data.width * 50,data.height * 50,"fafouri");
-    img = mlx_xpm_file_to_image(data.mlx,"./images/p1.xpm",&data.width,&data.height);
-    mlx_put_image_to_window(data.mlx,data.win,img,5,6);
+    //
+    img = mlx_xpm_file_to_image(data.mlx,"./sprites/Pac-Man/pac_semi_right.xpm",&data.width,&data.height);
+    wall = mlx_xpm_file_to_image(data.mlx, "./sprites/Other/Walls/wall.xpm", &data.width, &data.height);
+    //
+    mlx_put_image_to_window(data.mlx,data.win,wall,0,0);
+    mlx_put_image_to_window(data.mlx,data.win,wall,0,1);
+    mlx_put_image_to_window(data.mlx,data.win,wall,0,2);
+    mlx_put_image_to_window(data.mlx,data.win,wall,0,3);
+
+    mlx_put_image_to_window(data.mlx,data.win,img,15,30);
     mlx_loop(data.mlx);
     free(str);
     return 0;

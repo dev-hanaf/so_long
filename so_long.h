e01570eb08ba297e5ccc2ec5070e05fff909980a
Ayoub Hanaf <ahanaf@e1r11p9.1337.ma>
2024-03-27 17:18:22 +0000

    before started on leaks handling

--- so_long.h
+++ so_long.h
@@ -6,19 +6,16 @@
 /*   By: ahanaf <ahanaf@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/16 09:40:30 by ahanaf            #+#    #+#             */
-/*   Updated: 2024/03/26 07:04:23 by ahanaf           ###   ########.fr       */
+/*   Updated: 2024/03/27 17:07:47 by ahanaf           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SO_LONG_H
-#define SO_LONG_H
+# define SO_LONG_H
 
-/////////////////////////
-#include "./includes/get_next_line/get_next_line.h"
-#include "./includes/ft_printf/ft_printf.h"
-#include "./includes/libft/libft.h"
-
-////////////////////////
+# include "./includes/ft_printf/ft_printf.h"
+# include "./includes/get_next_line/get_next_line.h"
+# include "./includes/libft/libft.h"
 
 # define NC "\033[0m"
 # define RED "\033[31m"
@@ -28,54 +25,53 @@
 # define MAGENTA "\033[35m"
 # define CYAN "\033[36m"
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <mlx.h>
-#include <unistd.h>
-#include <fcntl.h>
+# include <fcntl.h>
+# include <mlx.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
 
-
-#define ESC     53
-#define W       13
-#define A	    0
-#define S	    1
-#define D	    2
+# define ESC 53
+# define W 13
+# define A 0
+# define S 1
+# define D 2
 
 typedef struct s_dimo
 {
-    void  *mlx;
-    void *win;
-    int height;
-    int width;
-    char **map;
-    int x;
-    int y;
-    int p;
-    int e;
-    int c;
-    int count_moves;
-    void *player;
-    void *wall;
-    void *door;
-    void *coll;
-}t_dimo;
+	void	*mlx;
+	void	*win;
+	int		height;
+	int		width;
+	char	**map;
+	int		x;
+	int		y;
+	int		p;
+	int		e;
+	int		c;
+	int		count_moves;
+	void	*player;
+	void	*wall;
+	void	*door;
+	void	*coll;
+}			t_dimo;
 
-char  **ft_split_so_long(char *str, t_dimo *dimo ,char sep);
-char	*get_all_map_lines(char **av);
-void	ft_exit_w_message(char *message);
-void height_map(t_dimo *data);
+char		**ft_split_so_long(char *str, t_dimo *dimo, char sep);
+char		*get_all_map_lines(char **av);
+void		ft_exit_w_message(char *message);
+void		height_map(t_dimo *data);
 
-void move_right(t_dimo *data);
-void move_down(t_dimo *data);
-void move_left(t_dimo *data);
-void move_up(t_dimo *data);
-void	valid_lenght(t_dimo *data);
-void	only_pce01(t_dimo *data, int i, int j);
-void	check_map(t_dimo *data);
-void flood_fill(t_dimo *data, int j, int i);
-void check_valid_flood_fill(char *str,t_dimo *data);
-void count_collectibles(t_dimo *data);
-void draw(t_dimo *data);
-int keys(int keycode, t_dimo *data);
-void initialize_mlx_vars(t_dimo *data);
+void		move_right(t_dimo *data);
+void		move_down(t_dimo *data);
+void		move_left(t_dimo *data);
+void		move_up(t_dimo *data);
+void		valid_lenght(t_dimo *data);
+void		only_pce01(t_dimo *data, int i, int j);
+void		check_map(t_dimo *data);
+void		flood_fill(t_dimo *data, int j, int i);
+void		check_valid_flood_fill(char *str, t_dimo *data);
+void		count_collectibles(t_dimo *data);
+void		draw(t_dimo *data);
+int			keys(int keycode, t_dimo *data);
+void		initialize_mlx_vars(t_dimo *data);
 #endif
